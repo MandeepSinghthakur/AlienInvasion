9f4a38ba76f0d7d4b0b1847068f2f0b3ccdddf83
Mandeep Singh <mandeep419.singh@gmail.com>
2021-10-31 18:19:34 -0600

    initial game setup

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+	# A class to store all the settings for Alien Invasion 
+
+	def __init__(self):
+		"""Initilialize the game settings"""
+		# Screen Settings
+		self.screen_width = 1200
+		self.screen_height = 500
+		self.bg_color = (230,230,230)
+		self.ship_speed = 1.5
